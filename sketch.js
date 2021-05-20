dead686d45c7b3ade3794d4629e48710ca0d9fd9
Madhura Yuvaraj Badhe <75791284+Madhura247@users.noreply.github.com>
2021-05-20 11:40:51 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -10,13 +10,10 @@ var walking_frame;
 var thunderCreatedFrame;
 
 function preload(){
-
     thunder1 = loadImage("images/thunderbolt/1.png");
     thunder2 = loadImage("images/thunderbolt/2.png");
     thunder3 = loadImage("images/thunderbolt/3.png");
     thunder4 = loadImage("images/thunderbolt/4.png");
-
-    walking_frame = loadAnimation("images/Walking Frame/walking_1.png", "images/Walking Frame/walking_2.png", "images/Walking Frame/walking_3.png", "images/Walking Frame/walking_4.png", "images/Walking Frame/walking_5.png", "images/Walking Frame/walking_6.png", "images/Walking Frame/walking_7.png" , "images/Walking Frame/walking_8.png")
 }
 
 function setup(){
@@ -32,8 +29,7 @@ function setup(){
     }
 
     umbrella = new Umbrella(200, 500);
-    umbrella.addAnimation ("walking", walking_frame);
-
+  
 }
 
 function draw(){
@@ -71,12 +67,6 @@ function draw(){
             thunder.destroy(); 
         }
         
-
-    
-
-    
-
     umbrella.display();
     drawSprites();
 }   
-
